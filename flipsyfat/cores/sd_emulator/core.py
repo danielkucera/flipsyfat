7f358304180d076dfc34aba7f61f43c071052422
Micah Elizabeth Scott <micah@misc.name>
2017-02-28 16:05:13 -0800

    More sensible I/O assignment

--- flipsyfat/cores/sd_emulator/core.py
+++ flipsyfat/cores/sd_emulator/core.py
@@ -4,5 +4,8 @@ from misoc.interconnect.csr import *
 
 class SDEmulator(Module, AutoCSR):
 	def __init__(self, slot):
+		counter = Signal(16)
 		self.slot = slot
+		self._foo = CSRStatus(len(counter))
+		self.sync += counter.eq(counter + slot.clk)
 
--- flipsyfat/targets/papilio_pro.py
+++ flipsyfat/targets/papilio_pro.py
@@ -5,40 +5,24 @@ import argparse
 from migen import *
 from flipsyfat.cores.sd_emulator import SDEmulator
 from misoc.targets.papilio_pro import BaseSoC
+from migen.build.generic_platform import *
 from misoc.integration.soc_sdram import *
 from misoc.integration.builder import *
 
-
-class PapilioWing:
-    def __init__(self, platform, wing):
-        if len(wing) == 1:
-            width = 16
-            first = 0
-        elif len(wing) == 2 and wing.endswith('L'):
-            # Low half
-            width = 8
-            first = 0
-        elif len(wing) == 2 and wing.endswith('H'):
-            width = 8
-            first = 8
-        else:
-            raise ValueError('Badly formed Wing name')
-        b = ['%s:%d' % (wing[0], first+i) for i in range(width)]
-        self.io = platform.constraint_manager.connector_manager.resolve_identifiers(b)
-
-
-class SDEmulatorWing(PapilioWing):
-    def __init__(self, platform, wing):
-        PapilioWing.__init__(self, platform, wing)
-        self.clk = self.io[0]
-        self.cmd = self.io[1]
-        self.dat = self.io[2:6]
-
+io = [
+    ("sdemu", 0,
+        Subsignal("clk", Pins("C:8")),
+        Subsignal("cmd", Pins("C:9")),
+        Subsignal("d", Pins("C:10 C:11 C:12 C:13")),
+    )
+]
 
 class Flipsyfat(BaseSoC):
     def __init__(self, **kwargs):
         BaseSoC.__init__(self, **kwargs)
-        self.submodules.sd_emulator = SDEmulator(SDEmulatorWing(self.platform, 'CL'))
+        self.platform.add_extension(io)
+        self.submodules.sdemu = SDEmulator(self.platform.request("sdemu"))
+        self.csr_devices += ["sdemu"]
 
 
 def main():
