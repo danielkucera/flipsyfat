67ee7fe11fa19025e1b56a77652cbfe790e8a782
Micah Elizabeth Scott <micah@misc.name>
2017-03-07 15:46:55 -0800

    SD core fixes from initial simulation

--- flipsyfat/cores/sd_emulator/verilog/sd_common.v
+++ flipsyfat/cores/sd_emulator/verilog/sd_common.v
@@ -39,6 +39,25 @@ endmodule
 module synch_3 #(parameter WIDTH = 1) (
    input  wire [WIDTH-1:0] i,     // input signal
    output reg  [WIDTH-1:0] o,     // synchronized output
+   input  wire             clk    // clock to synchronize on
+);
+
+reg [WIDTH-1:0] stage_1;
+reg [WIDTH-1:0] stage_2;
+reg [WIDTH-1:0] stage_3;
+
+always @(posedge clk) 
+   {stage_3, o, stage_2, stage_1} <= {o, stage_2, stage_1, i};
+   
+endmodule
+
+
+//
+// 3-stage synchronizer and rising edge detector
+//
+module synch_3r #(parameter WIDTH = 1) (
+   input  wire [WIDTH-1:0] i,     // input signal
+   output reg  [WIDTH-1:0] o,     // synchronized output
    input  wire             clk,   // clock to synchronize on
    output wire             rise   // one-cycle rising edge pulse
 );
--- flipsyfat/cores/sd_emulator/verilog/sd_link.v
+++ flipsyfat/cores/sd_emulator/verilog/sd_link.v
@@ -185,13 +185,13 @@ wire        data_out_done_s, data_out_done_r;
 synch_3       a(reset_n, reset_s, clk_50);
 synch_3 #(48) b(phy_cmd_in, cmd_in_s, clk_50);
 synch_3       c(phy_cmd_in_crc_good, cmd_in_crc_good_s, clk_50);
-synch_3       d(phy_cmd_in_act, cmd_in_act_s, clk_50, cmd_in_act_r);
+synch_3r      d(phy_cmd_in_act, cmd_in_act_s, clk_50, cmd_in_act_r);
 synch_3       e(phy_data_in_busy, data_in_busy_s, clk_50);
-synch_3       f(phy_data_in_done, data_in_done_s, clk_50, data_in_done_r);
+synch_3r      f(phy_data_in_done, data_in_done_s, clk_50, data_in_done_r);
 synch_3       g(phy_data_in_crc_good, data_in_crc_good_s, clk_50);
-synch_3       h(phy_resp_done, resp_done_s, clk_50, resp_done_r);
+synch_3r      h(phy_resp_done, resp_done_s, clk_50, resp_done_r);
 synch_3       i(phy_data_out_busy, data_out_busy_s, clk_50);
-synch_3       j(phy_data_out_done, data_out_done_s, clk_50, data_out_done_r);
+synch_3r      j(phy_data_out_done, data_out_done_s, clk_50, data_out_done_r);
 synch_3       k(phy_spi_sel, spi_sel_s, clk_50);
 
 
