b52b3452ac42405f04a74b7391df47e01f3b3bff
Daniel Kucera <daniel.kucera@gmail.com>
2018-06-28 22:30:56 +0200

    removed FAT16 implementation

--- flipsyfat/software/sdstream/main.c
+++ flipsyfat/software/sdstream/main.c
@@ -9,7 +9,6 @@
 #include <console.h>
 #include <system.h>
 
-#include "fat.h"
 #include "sdemu.h"
 #include "sdtimer.h"
 
@@ -35,24 +34,12 @@ int main(void)
     return 0;
 }
 
-void fat_rootdir_entry(uint8_t* dest, unsigned index)
+void block_read(uint8_t *buf, uint32_t lba)
 {
-    memset(dest, 0, 32);
-    if (index == 0) {
-        fat_volume_label(dest);
-    } else {
-        char name[9];
-
-        // Make up a filename!
-        sprintf(name, "F%d", index);
-
-        // Each file will be one cluster, at an arbitrary address
-        fat_plain_file(dest, name, "TXT", 0x100 + index, BLOCK_SIZE * FAT_CLUSTER_SIZE);
-    }
+    // Ignored
 }
 
-void fat_data_block(uint8_t* dest, unsigned cluster, unsigned index)
+void block_write(uint8_t *buf, uint32_t lba)
 {
-    memset(dest, 'Z', BLOCK_SIZE); 
-    sprintf((char*) dest, "%04x+%x cluster\n", cluster, index);
+    // Ignored
 }
